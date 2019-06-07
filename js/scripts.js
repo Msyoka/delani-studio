f47b4c7ba08e8d907ba2add9aa408bfabbcd3ae6
Msyoka <daviemumo37@gmail.com>
2019-06-07 11:16:06 +0300

    add jquery toggle and fuction click

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,14 @@
 $(document).ready(function() {
-      $(".jumbotron").click(function() {
+      $(".jumbotron#click").click(function() {
         $(".show").toggle();
         $(".hide").toggle();
+      });
+      $(".jumbotron#click1").click(function() {
+        $(".show1").toggle();
+        $(".hide1").toggle();
+      });
+      $(".jumbotron#click2").click(function() {
+        $(".show2").toggle();
+        $(".hide2").toggle();
       });
     });
